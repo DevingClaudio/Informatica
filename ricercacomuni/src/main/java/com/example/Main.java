2702901c0b1468a509d0bb44fdc861c1f9a9da81
FRANCO CLAUDIO MOLICA <molica.francoclaudio@S18-13.torricelli.local>
2025-10-01 11:32:45 +0200

    a

--- ricercacomuni/src/main/java/com/example/Main.java
+++ ricercacomuni/src/main/java/com/example/Main.java
@@ -1,7 +1,28 @@
 package com.example;
-
+import com.opencsv.CSVReader;
+import java.util.*;
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+      try {
+            CSVReader reader = new CSVReader(new FileReader("listacomuni.csv"));
+            String[] riga;
+            Scanner scan = new Scanner(System.in);
+            String ISTAT = scan.nextLine();
+            scan.close();
+            while((riga = reader.readNext()) != null) {
+                String comune = "";
+                String[] split = riga[0].split(";");
+                if (split[0].equals(ISTAT)) {
+                    for (int i = 0; i < 9; i++) {
+                        comune += split[i] + ";";
+                    }
+                    System.out.println(comune);
+                }
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }   
+    }   
 }
